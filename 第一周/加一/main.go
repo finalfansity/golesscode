83431d92afc6d3be63b068efe10fa860b9468953
zhuhui <douma515253@gmail.com>
2021-10-05 17:38:25 +0800

    add 001-02

--- 第一周/加一/main.go
+++ 第一周/加一/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func plusOne(digits []int) []int {
 	for i:= len(digits)-1;i>=0; i--{
 		digits[i] = digits[i] + 1
@@ -11,9 +9,4 @@ func plusOne(digits []int) []int {
 		}
 	}
 	return append([]int{1}, digits...)
-}
-
-func main()  {
-	list := []int{1,2,3}
-	fmt.Println(plusOne(list))
 }
